ea8a18df5b16c06112950529b748c0a65a7bbaf7
RoboErik <roboerik@android.com>
2015-07-24 14:01:40 -0700

    Added a set of simple scripts for making workflow easier
    This adds an envsetup.sh script to set up the workspace and to add
    a bgit command to your environment's path. bgit provides several
    commands to make working with github easier and to avoid conflicts
    with other developers.

--- app/src/main/java/com/google/blockly/MainActivity.java
+++ app/src/main/java/com/google/blockly/MainActivity.java
@@ -16,22 +16,17 @@
 package com.google.blockly;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity
