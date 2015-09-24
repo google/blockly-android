95267768aafe8f5bd43ac145c17a203a99eba3b5
Rachel Fenichel <fenichel@google.com>
2015-09-24 16:09:10 -0700

    remove newline

--- app/src/main/java/com/google/blockly/MainActivity.java
+++ app/src/main/java/com/google/blockly/MainActivity.java
@@ -157,8 +157,7 @@ public class MainActivity extends ActionBarActivity
                     getArguments().getInt(ARG_SECTION_NUMBER));
             mWorkspace = new Workspace();
         }
-
-
+        
         /**
          * Returns a new instance of this fragment for the given section
          * number.
