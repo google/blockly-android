70bc0dd56ef08369f8cdc49e256f37c4030aff2b
Torsten Rohlfing <rohlfing@google.com>
2015-10-06 15:57:16 -0700

    Removed unused imports.

--- app/src/main/java/com/google/blockly/ui/WorkspaceView.java
+++ app/src/main/java/com/google/blockly/ui/WorkspaceView.java
@@ -16,7 +16,6 @@
 package com.google.blockly.ui;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
@@ -24,7 +23,6 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.google.blockly.model.Workspace;
 import com.google.blockly.model.WorkspacePoint;
