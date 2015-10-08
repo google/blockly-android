2bbe2d0579a62b5c2f4b9cfa80c7b84ffc5b982a
Torsten Rohlfing <rohlfing@google.com>
2015-10-08 14:02:34 -0700

    Remove unused import.

--- app/src/main/java/com/google/blockly/MainActivity.java
+++ app/src/main/java/com/google/blockly/MainActivity.java
@@ -27,7 +27,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.google.blockly.model.Workspace;
 import com.google.blockly.ui.WorkspaceView;
