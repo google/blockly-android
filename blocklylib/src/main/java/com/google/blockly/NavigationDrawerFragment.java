21f8a637c7e5037768208e3b25133c32c5db96d6
Andrew n marshall <Anm@anm.me>
2015-12-10 11:03:57 -0800

    Don't focus on field values upon startup. Don't open the navigation drawer on start. Removed unnecessary Activity tag in blocklylib manifest.
    Actually don't focus on field values upon startup.

--- blocklylib/src/main/java/com/google/blockly/NavigationDrawerFragment.java
+++ blocklylib/src/main/java/com/google/blockly/NavigationDrawerFragment.java
@@ -179,12 +179,6 @@ public class NavigationDrawerFragment extends Fragment {
             }
         };
 
-        // If the user hasn't 'learned' about the drawer, open it to introduce them to the drawer,
-        // per the navigation drawer design guidelines.
-        if (!mUserLearnedDrawer && !mFromSavedInstanceState) {
-            mDrawerLayout.openDrawer(mFragmentContainerView);
-        }
-
         // Defer code dependent on restoration of previous instance state.
         mDrawerLayout.post(new Runnable() {
             @Override
