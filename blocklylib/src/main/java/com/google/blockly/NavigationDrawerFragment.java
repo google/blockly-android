766d09c86249461467e68c62f75224c8b3b45e7d
Rachel Fenichel <fenichel@google.com>
2015-12-11 14:24:50 -0800

    Shuffle actions around and add icons.

--- blocklylib/src/main/java/com/google/blockly/NavigationDrawerFragment.java
+++ blocklylib/src/main/java/com/google/blockly/NavigationDrawerFragment.java
@@ -235,12 +235,6 @@ public class NavigationDrawerFragment extends Fragment {
         if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-
-        if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
