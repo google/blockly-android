4f590ecd1240a81c7bdb235d8ce9db26114f92a5
Rachel Fenichel <fenichel@google.com>
2016-01-06 12:23:52 -0800

    fix touch targets near inputs

--- blocklylib/src/main/java/com/google/blockly/ui/InputView.java
+++ blocklylib/src/main/java/com/google/blockly/ui/InputView.java
@@ -198,11 +198,12 @@ public class InputView extends ViewGroup {
     }
 
     /**
-     * @return True if and only if a coordinate is on the fields of this view (any connected inputs
-     * should handle events themselves and are thus not allowed here).
+     * @return True if and only if a coordinate is on the fields of this view, including the padding
+     * to the left and right of the fields.  Any connected inputs should handle events themselves
+     * and are thus not allowed here.
      */
     public boolean isOnFields(int eventX, int eventY) {
-        return (eventX >= 0 && eventX < mFieldLayoutWidth) &&
+        return (eventX >= 0 && eventX < (mFieldLayoutWidth + mPatchManager.mBlockTotalPaddingX)) &&
                 eventY >= 0 && eventY < mRowHeight;
     }
 
