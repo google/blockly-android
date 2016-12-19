7abcf86f1c5771bbb29f2558cf8fca775a8e0875
chris.arnott@softwire.com <chris.arnott@softwire.com>
2016-12-19 22:20:14 +0000

    Tidy up Assert.fail to use stanadard assertions.

--- blocklytest/src/androidTest/java/com/google/blockly/utils/MoreAsserts.java
+++ blocklytest/src/androidTest/java/com/google/blockly/utils/MoreAsserts.java
@@ -2,15 +2,15 @@ package com.google.blockly.utils;
 
 import junit.framework.Assert;
 
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
 /**
  * Aseerts used by Blockly's test code.
  */
 public class MoreAsserts {
     public static void assertStringNotEmpty(String mesg, String str) {
-        if (str == null) {
-            Assert.fail(mesg + " Found null string.");
-        } else if(str.length() == 0) {
-            Assert.fail(mesg + " Found empty string.");
-        }
+        assertNotNull(mesg + " Found null string.", str);
+        assertNotEquals(mesg + " Found empty string.", str.length(), 0);
     }
 }
