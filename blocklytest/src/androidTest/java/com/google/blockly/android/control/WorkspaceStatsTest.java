416c8d0c042d850309cc36d2b47b895bde5fd435
Andrew n marshall <Anm@anm.me>
2016-12-21 12:02:17 -0800

    Removing wildcard imports.

--- blocklytest/src/androidTest/java/com/google/blockly/android/control/WorkspaceStatsTest.java
+++ blocklytest/src/androidTest/java/com/google/blockly/android/control/WorkspaceStatsTest.java
@@ -31,8 +31,11 @@ import org.junit.Test;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-import static org.junit.Assert.*;
 
 /**
  * Tests for {@link WorkspaceStats}.
