42e0b77153284e83189bf8f53d5bad3861fe85e7
Andrew n marshall <Anm@anm.me>
2017-05-09 16:25:30 -0700

    Unused import

--- blocklylib-core/src/main/java/com/google/blockly/android/ui/BlockViewFactory.java
+++ blocklylib-core/src/main/java/com/google/blockly/android/ui/BlockViewFactory.java
@@ -20,7 +20,6 @@ import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v13.view.ViewCompat;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
