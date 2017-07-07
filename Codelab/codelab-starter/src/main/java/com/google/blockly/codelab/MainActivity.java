2b9b6b08ee93acfaee65de2f4d1c24f04a341c09
Andrew n marshall <Anm@anm.me>
2017-07-07 14:16:33 -0700

    Removing unused constants and mMode variable.

--- Codelab/codelab-starter/src/main/java/com/google/blockly/codelab/MainActivity.java
+++ Codelab/codelab-starter/src/main/java/com/google/blockly/codelab/MainActivity.java
@@ -1,30 +1,16 @@
 package com.google.blockly.codelab;
 
-import android.support.annotation.IntDef;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.lang.annotation.Retention;
-
-import static java.lang.annotation.RetentionPolicy.SOURCE;
-
 /**
  * The application's main (and only) activity, hosting the toolbar and content fragments.
  */
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MODE = "MODE";
-
-    @Retention(SOURCE)
-    @IntDef({MODE_PLAY, MODE_EDIT})
-    public @interface AppMode {}
-    public static final int MODE_PLAY = 1;
-    public static final int MODE_EDIT = 2;
-
-    private int mMode = MODE_PLAY;
-
+    // View and fragment references:
     private ActionBar mActionBar;
     private FragmentManager mFragmentManager;
     private PlaybackFragment mPlaybackFragment = new PlaybackFragment();
