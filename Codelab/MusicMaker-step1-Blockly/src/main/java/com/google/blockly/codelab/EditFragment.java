3e43124d6eb6344cbf9b25e7bf3d5cc624fb9370
Andrew n marshall <Anm@anm.me>
2017-08-23 11:06:53 -0700

    * Added button handling in EditFragment.  * Imported blockly dependency to step1 (and associated Android support files, from maven).  * Added BlocklyFragment, triggered by selecting one of the buttons on the EditFragment, with MusicMaker specific blocks, toolbox, and generators.  * Added copyrights to source files.

--- Codelab/MusicMaker-step1-Blockly/src/main/java/com/google/blockly/codelab/EditFragment.java
+++ Codelab/MusicMaker-step1-Blockly/src/main/java/com/google/blockly/codelab/EditFragment.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.blockly.codelab;
 
 import android.os.Bundle;
@@ -8,15 +23,41 @@ import android.view.View;
 import android.view.ViewGroup;
 
 /**
- * An array of colorful buttons, with some instructional text and an "Edit" menu option.
+ * An array of colorful buttons, matching the PlaybackFragment equivalents. Clicking any button will
+ * open the matching editor.
  */
 public class EditFragment extends Fragment {
-    @Nullable
     @Override
     public View onCreateView(
             LayoutInflater inflater,
             @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.edit_fragment, null);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.edit_fragment, null);
+
+        // Each button edits a different script.
+        rootView.findViewById(R.id.button1).setOnClickListener(buildEditOnClick(1));
+        rootView.findViewById(R.id.button2).setOnClickListener(buildEditOnClick(2));
+        rootView.findViewById(R.id.button3).setOnClickListener(buildEditOnClick(3));
+        rootView.findViewById(R.id.button4).setOnClickListener(buildEditOnClick(4));
+        rootView.findViewById(R.id.button5).setOnClickListener(buildEditOnClick(5));
+        rootView.findViewById(R.id.button6).setOnClickListener(buildEditOnClick(6));
+        rootView.findViewById(R.id.button7).setOnClickListener(buildEditOnClick(7));
+        rootView.findViewById(R.id.button8).setOnClickListener(buildEditOnClick(8));
+        rootView.findViewById(R.id.button9).setOnClickListener(buildEditOnClick(9));
+
+        return rootView;
+    }
+
+    protected void onEdit(int buttonNumber) {
+        ((MainActivity) getActivity()).editSoundScript(buttonNumber);
+    }
+
+    protected View.OnClickListener buildEditOnClick(final int buttonNumber) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onEdit(buttonNumber);
+            }
+        };
     }
 }
