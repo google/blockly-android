25bf98b1db65a49b792f78e222e05e78d011d531
Andrew n marshall <Anm@anm.me>
2017-08-30 10:25:07 -0700

    Adding clarifying comments to the save path overrides.

--- blocklydemo/src/main/java/com/google/blockly/android/demo/SplitActivity.java
+++ blocklydemo/src/main/java/com/google/blockly/android/demo/SplitActivity.java
@@ -143,12 +143,22 @@ public class SplitActivity extends AbstractBlocklyActivity {
         mGeneratedTextView.setMinWidth((int) (maxline * 13 * density));
     }
 
+    /**
+     * Optional override of the save path, since this demo Activity has multiple Blockly
+     * configurations.
+     * @return Workspace save path used by this Activity.
+     */
     @Override
     @NonNull
     protected String getWorkspaceSavePath() {
         return SAVE_FILENAME;
     }
 
+    /**
+     * Optional override of the auto-save path, since this demo Activity has multiple Blockly
+     * configurations.
+     * @return Workspace auto-save path used by this Activity.
+     */
     @Override
     @NonNull
     protected String getWorkspaceAutosavePath() {
