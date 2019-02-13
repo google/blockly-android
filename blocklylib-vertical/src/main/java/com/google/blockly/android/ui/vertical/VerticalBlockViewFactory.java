4851de12db2fc5b5ab5667d62e5f74d58922a185
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2019-02-13 13:08:40 -0500

    Prettify Dialogs, Disable Accessing Mutator UI in Flyout (#764)
    Merged from TheBrokenRail/prettify-dialogs

--- blocklylib-vertical/src/main/java/com/google/blockly/android/ui/vertical/VerticalBlockViewFactory.java
+++ blocklylib-vertical/src/main/java/com/google/blockly/android/ui/vertical/VerticalBlockViewFactory.java
@@ -210,7 +210,11 @@ public class VerticalBlockViewFactory extends BlockViewFactory<BlockView, InputV
                 }
             }
         };
-        return buildIconFieldView(listener, com.google.blockly.android.R.drawable.mutator_icon_24dp);
+        if (!block.getController().getWorkspace().isRootBlock(block.getRootBlock())) {
+            return buildIconFieldView(null, com.google.blockly.android.R.drawable.mutator_icon_24dp);
+        } else {
+            return buildIconFieldView(listener, com.google.blockly.android.R.drawable.mutator_icon_24dp);
+        }
     }
 
     protected View buildIconFieldView(View.OnClickListener listener, int resId) {
